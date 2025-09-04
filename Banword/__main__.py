454199581041de018771c76ab35f25648ea42966
shivmish8090 <apilania55@gmail.com>
2025-09-04 09:33:10 +0530

    Update __main__.py

--- Banword/__main__.py
+++ Banword/__main__.py
@@ -10,10 +10,10 @@ loop = asyncio.get_event_loop()
 async def roy_bot():
     for all_module in ALL_MODULES:
         importlib.import_module("Banword.modules." + all_module)
-    print("• @aaradhyavideBot B𝗈𝗍 Started Successfully.")
+    print("• @EditGuardiansBot B𝗈𝗍 Started Successfully.")
     await idle()
-    print("• Don't edit baby, otherwise you get an error: @networkxlog")
-    await Banword.send_message(LOGGER_ID, "**✦ ɪ ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ.\n\n✦ ᴊᴏɪɴ - @aashikteam**")
+    print("• Don't edit baby, otherwise you get an error: @dns_support_group")
+    await Banword.send_message(LOGGER_ID, "**✦ ɪ ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ.\n\n✦ ᴊᴏɪɴ - @Team_Dns_Network**")
 
 if __name__ == "__main__":
     loop.run_until_complete(roy_bot())
